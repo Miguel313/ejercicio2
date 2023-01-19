3fe88a9307d54cd1986998d9b66b5764f166e217
Miguel313 <maherrojog01@iesarroyoharnina.es>
2023-01-19 17:44:16 +0100

    ejercicio2

---
+++ src/Ejemplo1/Ejercicio2.java
@@ -0,0 +1,9 @@
+package Ejemplo1;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		String a="papadopaulus";
+		System.out.println(a);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rmigu
+ *
+ */
+module Prueba1 {
+}
